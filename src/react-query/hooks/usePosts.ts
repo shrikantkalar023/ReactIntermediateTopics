221d812d5b0063214da878af0d06984005138d6f
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-05-03 12:55:28 +0530

    learnt about paginated queries

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -8,19 +8,25 @@ interface Post {
   userId: number;
 }
 
-const usePosts = (userId: number | undefined) =>
+interface PostQuery {
+  page: number;
+  pageSize: number;
+}
+
+const usePosts = (query: PostQuery) =>
   useQuery<Post[], Error>({
-    // here userID is parameter for this query. similar to dependencies array in useEffect.
-    queryKey: userId ? ["users", userId, "posts"] : ["posts"],
+    queryKey: ["posts", query],
     queryFn: () =>
       axios
         .get("https://jsonplaceholder.typicode.com/posts", {
           params: {
-            userId,
+            _start: (query.page - 1) * query.pageSize,
+            _limit: query.pageSize,
           },
         })
         .then((res) => res.data),
     staleTime: 1 * 60 * 1000, //1 min
+    keepPreviousData: true,
   });
 
 export default usePosts;
