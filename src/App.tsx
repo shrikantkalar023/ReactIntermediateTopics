d0cdd6aacef5fddc7a982fa4a2274300fd00f654
Shrikant Kalar <shrikantkalar023@gmail.com>
2024-04-15 00:27:44 +0530

    custom hook for accessing context

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useReducer } from "react";
 import "./App.css";
 import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
-import AuthProvider from "./state-management/authProvider";
+import AuthProvider from "./state-management/AuthProvider";
 import TasksContext from "./state-management/contexts/tasksContext";
 import tasksReducer from "./state-management/reducers/tasksReducer";
 
